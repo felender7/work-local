9bac5bc41a2f7c32bd0f4c8249ed9c058fc57ab2
Felender Hlungwani <felender7@gmail.com>
2019-08-23 11:55:30 +0200

    stripe set up

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['stripe_publishable_key'],
-  :secret_key      => ENV['stripe_api_key']
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key:      ENV['STRIPE_SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:stripe_publishable_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
+STRIPE_PUBLIC_KEY = Rails.configuration.stripe[:publishable_key]
